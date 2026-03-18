f0d3b523b13d4dc85f1be46db79ed879439d1b32
vitorma28 <vimat28.3.1@gmail.com>
2026-03-17 22:54:57 -0300

    Começando a criação das rotas principais.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const sequelize = require('./config/database');
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/user');
+const mainRoutes = require('./routes/main');
 
 const app = express();
 app.use(express.json());
 
 // Usando as rotas de usuário
 app.use('/usuarios', userRoutes);
+app.use('/', mainRoutes);
 
 sequelize.sync().then(() => {
   app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
