79fbf929722527c42646404adbc9257081675940
vitorma28 <vimat28.3.1@gmail.com>
2026-04-14 23:26:30 -0300

    Preparando as rotas de controle de usuário.

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const User = require('../models/User.js');
+const { verifyJWT } = require('../middlewares/verifyJWT.js');
 
 const router = express.Router();
 
-router.patch('/:id', (req, res) => {});
+router.patch('/:id', verifyJWT, async (req, res) => {});
+
+router.delete('/:id', verifyJWT, async (req, res) => {});
+
+router.get('/', verifyJWT, async (req, res) => {});
 
 module.exports = router;
